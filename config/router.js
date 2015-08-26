d350e3c1f697e331d0018f7554faece11e971183
Josif78 <seph12.alc@gmail.com>
2015-08-26 12:23:09 +0800

    Added mysql attachment

--- config/router.js
+++ config/router.js
@@ -3,15 +3,21 @@ var teacher = require('./../controllers/teacher');
 
 module.exports = function(router) {
        router.route('/students')
-              .get(student.find);
+              .get(student.find)
+              .post(student.insert);
+              
+       router.route('/students/:id') 
+              .get(student.findOne)
+              .put(student.update)
+              .delete(student.remove);
               
        return router;
 };
 
-module.exports = function(router) {
+/*module.exports = function(router) {
        router.route('/teachers')
               .get(teacher.find)
               .post(teacher.add);
               
        return router;
-};
+};*/
--- controllers/student.js
+++ controllers/student.js
@@ -1,3 +1,55 @@
-exports.find = function(req, res) {
-       res.send('Pururut~');
+/*exports.find = function(req, res) {
+	res.send('Pururut~');
+};*/
+
+var db = require(__dirname + '/../lib/mysql');
+
+exports.find = function(req, res, next) {
+	db.query("select * from student", function(err, rows){
+		if(err) return next(err);
+		res.send(rows);
+	});
+};
+
+exports.findOne = function(req, res, next) {
+	db.query("select *from student where id = ?", [req.params.id], function(err, rows) {
+		if(err) return next(err);
+		if(rows.length === 0) {
+			res.status(404).send("Student Not Found");
+		}
+		else {
+			res.send(rows[0]);
+		}
+	});
+};
+
+exports.insert = function(req, res, next) {
+	db.query("insert into student(studno, name) values (?,?)", [req.body.studno, req.body.name], function(err, rows) {
+		if(err) return next(err);
+		res.send(rows);
+	});
+};
+
+exports.update = function(req, res, next) {
+	db.query("update student set ? where id = ?", [req.body, req.params.id], function(err, next) {
+		if(err) return next(err);
+		if(rows.length === 0) {
+			res.status(404).send("Student Not Found");
+		}
+		else {
+			res.send(rows[0]);
+		}
+	});
+};
+
+exports.remove = function(req, res, next) {
+	db.query("delete from student where id = ?", [req.params.id], function(err, rows) {
+		if(err) return next(err);
+		if(rows.length === 0) {
+			res.status(404).send("Student Not Found");
+		}
+		else {
+			res.send(rows[0]);
+		}
+	});
 };
